5860bd08c3c79f3494d9c96fd8c102ea2af85333
feb-1 <92838628+feb-1@users.noreply.github.com>
2021-12-06 12:00:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,10 @@ function App() {
     axios
       .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=50&page=1&sparkline=false")
       .then((res) => {
-        console.log(res);
         setCoins(res.data);
       })
       .catch((err) => {
         console.log(err);
-        alert("Yo error!!!");
       });
   }, []);
 
